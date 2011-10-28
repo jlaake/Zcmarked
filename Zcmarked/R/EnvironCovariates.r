88a50d9f738768b4e3826aff0c90650de9178229
Jeff Laake <jefflaake@gmail.com>
2011-10-28 15:22:11 -0700

    Added OctoMar anomalies in environmental data; use access data from library location and modified suckling ch to use u and S for miss-classification model.

--- Zcmarked/R/EnvironCovariates.r
+++ Zcmarked/R/EnvironCovariates.r
@@ -28,6 +28,7 @@ EnvironCovariates<-function(average.years=c(1994:1996,1998:2008),fdir="",sites=1
 	SSTAnomalies[is.nan(SSTAnomalies)]=NA
 	nyears=nrow(SSTAnomalies)
 	OcttoJuneSSTAnomalies=rowMeans(cbind(SSTAnomalies[1:(nyears-1),c("Oct","Nov","Dec")],SSTAnomalies[2:nyears,c("Jan","Feb","Mar","Apr","May","June")]),na.rm=TRUE)
+	OcttoMarSSTAnomalies=rowMeans(cbind(SSTAnomalies[1:(nyears-1),c("Oct","Nov","Dec")],SSTAnomalies[2:nyears,c("Jan","Feb","Mar")]),na.rm=TRUE)
 	ApriltoJuneSSTAnomalies=rowMeans(SSTAnomalies[,c("Apr","May","June")],na.rm=TRUE)
 	names(ApriltoJuneSSTAnomalies)=as.character(as.numeric(names(ApriltoJuneSSTAnomalies))-1)
 	JulytoJuneSSTAnomalies=rowMeans(cbind(SSTAnomalies[1:(nyears-1),c("July","Aug","Sept","Oct","Nov","Dec")],SSTAnomalies[2:nyears,c("Jan","Feb","Mar","Apr","May","June")]),na.rm=TRUE)
@@ -39,20 +40,23 @@ EnvironCovariates<-function(average.years=c(1994:1996,1998:2008),fdir="",sites=1
 	maxyear=max(as.numeric(dimnames(UWI)[[1]]))
 	nyears=maxyear-minyear+1
 	OcttoJuneUWI33Anomalies=rowMeans(cbind(UWI[1:(nyears-1),as.character(10:12),1],UWI[2:nyears,as.character(1:6),1]),na.rm=TRUE)
+	OcttoMarUWI33Anomalies=rowMeans(cbind(UWI[1:(nyears-1),as.character(10:12),1],UWI[2:nyears,as.character(1:3),1]),na.rm=TRUE)
 	ApriltoJuneUWI33Anomalies=rowMeans(UWI[,as.character(4:6),1],na.rm=TRUE)[1:nyears]
 	names(ApriltoJuneUWI33Anomalies)=as.character(as.numeric(names(ApriltoJuneUWI33Anomalies))-1)
 	JulytoJuneUWI33Anomalies=rowMeans(cbind(UWI[1:(nyears-1),as.character(7:12),1],UWI[2:nyears,as.character(1:6),1]),na.rm=TRUE)
 	OcttoJuneUWI36Anomalies=rowMeans(cbind(UWI[1:(nyears-1),as.character(10:12),2],UWI[2:nyears,as.character(1:6),2]),na.rm=TRUE)
+	OcttoMarUWI36Anomalies=rowMeans(cbind(UWI[1:(nyears-1),as.character(10:12),2],UWI[2:nyears,as.character(1:3),2]),na.rm=TRUE)
 	ApriltoJuneUWI36Anomalies=rowMeans(UWI[,as.character(4:6),2],na.rm=TRUE)
 	names(ApriltoJuneUWI36Anomalies)=as.character(as.numeric(names(ApriltoJuneUWI36Anomalies))-1)
 	JulytoJuneUWI36Anomalies=rowMeans(cbind(UWI[1:(nyears-1),as.character(7:12),2],UWI[2:nyears,as.character(1:6),2]),na.rm=TRUE)
-#   Multivariate ENSO Index - lagged by 3 months
+#   Multivariate ENSO Index - lagged by 3 months -- so the indices don't line up with the months
 	MEI=sqlFetch(connection,"MEI")
 	minyear=min(MEI$Year)
 	maxyear=max(MEI$Year)
 	nyears=maxyear-minyear+1
 	MEI=tapply(MEI$MEI,list(MEI$Year,MEI$Month),unique)
 	OcttoJuneMEI=rowMeans(cbind(MEI[1:(nyears-1),as.character(7:12)],MEI[2:nyears,as.character(1:3)]),na.rm=TRUE)
+    OcttoMarMEI=rowMeans(cbind(MEI[1:(nyears-1),as.character(7:12)]),na.rm=TRUE)
 	ApriltoJuneMEI=rowMeans(MEI[,as.character(1:3)],na.rm=TRUE)
 	names(ApriltoJuneMEI)=as.character(as.numeric(names(ApriltoJuneMEI))-1)
 	JulytoJuneMEI=rowMeans(cbind(MEI[1:(nyears-1),as.character(4:12)],MEI[2:nyears,as.character(1:3)]),na.rm=TRUE)
@@ -62,15 +66,19 @@ EnvironCovariates<-function(average.years=c(1994:1996,1998:2008),fdir="",sites=1
 #   April-to-June is set as the previous year to model survival of pup cohort born in the previous year
     envcovdf=data.frame(time=1987:maxyear,
 			OcttoJuneSSTAnomalies=OcttoJuneSSTAnomalies[as.character(1987:maxyear)],
+			OcttoMarSSTAnomalies=OcttoMarSSTAnomalies[as.character(1987:maxyear)],	
 			ApriltoJuneSSTAnomalies=ApriltoJuneSSTAnomalies[as.character(1987:maxyear)],
 			JulytoJuneSSTAnomalies=JulytoJuneSSTAnomalies[as.character(1987:maxyear)],
 			OcttoJuneUWI33Anomalies=OcttoJuneUWI33Anomalies[as.character(1987:maxyear)],
+			OcttoMarUWI33Anomalies=OcttoMarUWI33Anomalies[as.character(1987:maxyear)],
 			ApriltoJuneUWI33Anomalies=ApriltoJuneUWI33Anomalies[as.character(1987:maxyear)],
 			JulytoJuneUWI33Anomalies=JulytoJuneUWI33Anomalies[as.character(1987:maxyear)],
 			OcttoJuneUWI36Anomalies=OcttoJuneUWI36Anomalies[as.character(1987:maxyear)],
+			OcttoMarUWI36Anomalies=OcttoMarUWI36Anomalies[as.character(1987:maxyear)],
 			ApriltoJuneUWI36Anomalies=ApriltoJuneUWI36Anomalies[as.character(1987:maxyear)],
 			JulytoJuneUWI36Anomalies=JulytoJuneUWI36Anomalies[as.character(1987:maxyear)],
 			OcttoJuneMEI=OcttoJuneMEI[as.character(1987:maxyear)],
+			OcttoMarMEI=OcttoMarMEI[as.character(1987:maxyear)],
 			ApriltoJuneMEI=ApriltoJuneMEI[as.character(1987:maxyear)],
 			JulytoJuneMEI=JulytoJuneMEI[as.character(1987:maxyear)]
 			)
